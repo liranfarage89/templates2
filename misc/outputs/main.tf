39b2069fd54314698e1e04e0ba737ab798010c18
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-03-27 16:13:59 +0300

    Create main.tf

---
+++ misc/outputs/main.tf
@@ -0,0 +1,23 @@
+
+variable "aws_default_region" { 
+	default = "us-east-1"
+	description = "AWS default region."
+}
+
+variable "fake_sensitive_variable" { 
+	default = "foo"
+	description = "sensitive decsription"
+	sensitive = true 
+}
+
+variable "fake_sensitive_variable" { 
+	default = "foo"
+	description = "sensitive decsription"
+	sensitive = true 
+}
+
+resource "null_resource" "null" {}
+
+output default_region {
+	value = var.aws_default_region
+}
