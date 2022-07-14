3f347c3561f62f572b4197c84530ec45c2067332
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-07-14 17:13:49 +0300

    Create main.tf

---
+++ misc/assumed-role/main.tf
@@ -0,0 +1,9 @@
+module "temporary_assumed_role" {
+  source = "./modules/iam_assume_role"
+  name = "test_liran_temporary"
+  iam_policies = [
+    "arn:aws:iam::aws:policy/AdministratorAccess"
+  ]
+  env0_aws_account_id = 170412844252 # dev account
+  external_id = var.env0_deployer_external_id
+}
