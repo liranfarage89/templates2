5eae6d4826c6833fca860d3a7065a6e4bff3993c
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-07-14 18:21:21 +0300

    Update main.tf

--- misc/assumed-role/iam_assumed_role/main.tf
+++ misc/assumed-role/iam_assumed_role/main.tf
@@ -1,14 +1,12 @@
-resource "aws_iam_role" "assume_role" {
-  name                = var.name
-  max_session_duration = var.max_session_duration
-  assume_role_policy = jsonencode({
+resource "local_file" "foo" {
+    content  = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::${var.env0_aws_account_id}:root"
+          AWS = "arn:aws:iam::23456:root"
         }
         Condition = {
           StringEquals = {
@@ -18,19 +16,7 @@ resource "aws_iam_role" "assume_role" {
       },
     ]
   })
+    filename = "${path.module}/foo.bar"
 }
 
-
-resource "aws_iam_role_policy_attachment" "policy_attach" {
-  for_each = local.iam_policies_map
-  role = aws_iam_role.assume_role.name
-  policy_arn = each.value
-
-}
-
-locals {
-  iam_policies_map = {
-  for idx, value in var.iam_policies: idx => value
-  }
-}
 
