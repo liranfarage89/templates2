82fe1961e60f4fea310c604b3ade627541a48a98
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-07-14 17:20:42 +0300

    Update main.tf

--- misc/assumed-role/main.tf
+++ misc/assumed-role/main.tf
@@ -2,7 +2,7 @@ module "temporary_assumed_role" {
   source = "./modules/iam_assumed_role"
   name = "test_liran_temporary"
   iam_policies = [
-    "arn:aws:iam::aws:policy/AdministratorAccess"
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
   ]
   env0_aws_account_id = 170412844252 # dev account
   external_id = var.env0_deployer_external_id
