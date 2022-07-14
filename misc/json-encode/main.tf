6ecf002b48f618fe88e9a19dcacde3fa2c6637bd
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-07-14 18:08:59 +0300

    Update main.tf

--- misc/json-encode/main.tf
+++ misc/json-encode/main.tf
@@ -9,6 +9,28 @@ variable "var2" {
 	description = "dummy var 2 descritpion"
 }
 
+resource "local_file" "foo" {
+    content  = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::23456:root"
+        }
+        Condition = {
+          StringEquals = {
+            "sts:ExternalId" = var.var2
+          }
+        }
+      },
+    ]
+  })
+    filename = "${path.module}/foo.bar"
+}
+
+
 output var1_output {
 	value = var.var1
 }
