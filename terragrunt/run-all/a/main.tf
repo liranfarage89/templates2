557b3b9180ae9f3ff76d2c21675258709103e35c
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-09-19 13:01:45 +0300

    Update main.tf

--- terragrunt/run-all/a/main.tf
+++ terragrunt/run-all/a/main.tf
@@ -1,5 +1,5 @@
 module "blueprints-private-tf-module" {
-  source = "api-bors.dev.env0.com/214afc56-607b-447a-a533-5f3f6d608539/blueprints-private-tf-module/env0"
+  source = "api-dev.dev.env0.com/e23f4e28-4304-49fd-a7cc-826a1952c3a8/blueprints-private-tf-module/env0"
   version = "1.0.1"
 }
 
