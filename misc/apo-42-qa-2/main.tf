56e0c7803141bba307f03b0d8f823cdeff208c57
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2026-06-11 15:23:00 +0300

    DEV-stage QA round 2 commit 2: revert apo-42-qa-2, change apo-42-qa

--- misc/apo-42-qa-2/main.tf
+++ misc/apo-42-qa-2/main.tf
@@ -1,2 +1,1 @@
 resource "null_resource" "test" {}
-# dev round2 commit1: touch apo-42-qa-2 (to be reverted)
--- misc/apo-42-qa/main.tf
+++ misc/apo-42-qa/main.tf
@@ -1,2 +1,2 @@
 resource "null_resource" "test" {}
-# dev-stage qa round 1: change only apo-42-qa
+# dev round2 commit2: change apo-42-qa only (net diff)
