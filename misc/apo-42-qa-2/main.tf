5472bfd74faf3eb8498dfd56560550973251fedc
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2026-06-11 12:45:44 +0300

    QA round 2 commit 2: revert apo-42-qa-2, change apo-42-qa

--- misc/apo-42-qa-2/main.tf
+++ misc/apo-42-qa-2/main.tf
@@ -1,2 +1,1 @@
 resource "null_resource" "test" {}
-# round2 commit1: touched apo-42-qa-2 (to be reverted)
--- misc/apo-42-qa/main.tf
+++ misc/apo-42-qa/main.tf
@@ -1,4 +1,2 @@
 resource "null_resource" "test" {}
-# qa round 1: change only apo-42-qa
-# any-change probe
-# probe after onVcsPush secret fix
+# round2 commit2: change apo-42-qa only (net diff)
