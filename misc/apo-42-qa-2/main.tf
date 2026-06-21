42bb7c0371a08b5ea35e5cbf0437588c13db5c2d
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2026-06-21 16:51:55 +0300

    C1: touch both apo-42-qa and apo-42-qa-2

--- misc/apo-42-qa-2/main.tf
+++ misc/apo-42-qa-2/main.tf
@@ -1,1 +1,2 @@
 resource "null_resource" "test" {}
+# apo-84 qa C1: transient touch to apo-42-qa-2 (reverted in C2)
--- misc/apo-42-qa/main.tf
+++ misc/apo-42-qa/main.tf
@@ -1,3 +1,4 @@
 resource "null_resource" "test" {}
 # re-qa round2 c2: change apo-42-qa only (net diff)
 #
+# apo-84 qa C1: net change to apo-42-qa
